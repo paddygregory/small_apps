bcb35a4975e25e676a55872a7b47a794028423c8
pgregory <paddygregory62@gmail.com>
2025-07-18 21:34:09 +1000

    initial endpoints

--- Book Review API/BookReviewAPI.py
+++ Book Review API/BookReviewAPI.py
@@ -1,3 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
+import uvicorn
+from typing import Optional
 
 app = FastAPI()
+
+
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    genre: str
+    rating : float
+
+books = { 
+    1: { "title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "genre": "Classic", "rating": 4.5 }
+}
+
+@app.post("/create-book")
+def create_book(book: Book):
+    return book
+
+@app.get("/get-book/{book_id}")
+def get_book(book_id: int = Path(..., description="The ID of the book you want to get")):
+    return books[book_id]
+
+@app.get("/get-book")
+def all_books():
+    return books
+
+@app.delete("/delete-book/{book_id}")
+def delete_book(book_id: int):
+    del books[book_id]
+    return {"message": "Book deleted successfully"}
